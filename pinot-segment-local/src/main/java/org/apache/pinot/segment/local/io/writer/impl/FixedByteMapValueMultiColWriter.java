2409ca02dddb2e864931167e951a155869cc0df1
Erich Ess <erich@startree.ai>
2023-12-13 09:58:35 -0800

    Updated variable naming to correct that the buffer stores the DocId to Value mapping not the Key to Value mapping

--- pinot-segment-local/src/main/java/org/apache/pinot/segment/local/io/writer/impl/FixedByteMapValueMultiColWriter.java
+++ pinot-segment-local/src/main/java/org/apache/pinot/segment/local/io/writer/impl/FixedByteMapValueMultiColWriter.java
@@ -24,21 +24,19 @@ import java.io.IOException;
 import java.nio.ByteOrder;
 import org.apache.pinot.segment.spi.memory.PinotDataBuffer;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-
 
 public class FixedByteMapValueMultiColWriter implements Closeable {
-  private final int _keyOffset;
+  private final int _docIdOffset;
   private final int _columnOffset;
   private final int _rowSizeInBytes;
   private final PinotDataBuffer _dataBuffer;
   private final boolean _shouldCloseDataBuffer;
 
-  public FixedByteMapValueMultiColWriter(File file, int rows, int keySize, int columnSize)
+  public FixedByteMapValueMultiColWriter(File file, int rows, int docIdSize, int columnSize)
       throws IOException {
-    _keyOffset = 0;
-    _columnOffset = _keyOffset + keySize;
-    _rowSizeInBytes = keySize + columnSize;
+    _docIdOffset = 0;
+    _columnOffset = _docIdOffset + docIdSize;
+    _rowSizeInBytes = docIdSize + columnSize;
     int totalSize = _rowSizeInBytes * rows;
     // Backward-compatible: index file is always big-endian
     _dataBuffer = PinotDataBuffer.mapFile(file, false, 0, totalSize, ByteOrder.BIG_ENDIAN, getClass().getSimpleName());
@@ -46,8 +44,8 @@ public class FixedByteMapValueMultiColWriter implements Closeable {
   }
 
   public FixedByteMapValueMultiColWriter(PinotDataBuffer dataBuffer, int keySize, int columnSize) {
-    _keyOffset = 0;
-    _columnOffset = _keyOffset + keySize;
+    _docIdOffset = 0;
+    _columnOffset = _docIdOffset + keySize;
     _rowSizeInBytes = keySize + columnSize;
     _dataBuffer = dataBuffer;
     // For passed in PinotDataBuffer, the caller is responsible for closing the PinotDataBuffer.
@@ -66,10 +64,10 @@ public class FixedByteMapValueMultiColWriter implements Closeable {
     return true;
   }
 
-  public void setInt(int row, int key, int i) {
-    int keyOffset = _rowSizeInBytes * row + _keyOffset;
+  public void setIntValue(int row, int docId, int i) {
+    int docIdOffset = _rowSizeInBytes * row + _docIdOffset;
     int valueOffset = _rowSizeInBytes * row + _columnOffset;
-    _dataBuffer.putInt(keyOffset, key);
+    _dataBuffer.putInt(docIdOffset, docId);
     _dataBuffer.putInt(valueOffset, i);
   }
 }
